ebef662537236c91cf2fbdfb63a71c1f8a5425fa
szymon-kurek <86785155+szymon-kurek@users.noreply.github.com>
2022-04-30 11:41:59 +0200

    added google Sign in styles

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { Button, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { GOOGLE_EXPO_CLIENT_ID } from '@env';
-
+import Logo from '../assets/images/googleIcon.svg';
 WebBrowser.maybeCompleteAuthSession();
 
 export function GoogleSignIn(props: any) {
-  console.log(props.navigation);
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: GOOGLE_EXPO_CLIENT_ID,
     // iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
@@ -19,21 +18,65 @@ export function GoogleSignIn(props: any) {
     if (response?.type === 'success') {
       const { authentication } = response;
 
-      console.log(authentication);
+      // console.log(authentication);
 
       props.navigation.navigate('Root');
     }
   }, [response]);
 
+  // https://github.com/react-native-google-signin/google-signin
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
+    <View style={styles.container}>
+      <Text style={styles.title}>Roomeo</Text>
+      <TouchableOpacity
         disabled={!request}
-        title="Login"
         onPress={() => {
           promptAsync();
         }}
-      />
+        style={styles.googleSignIn}
+      >
+        <TouchableOpacity style={styles.googleSignInLogo}>
+          <Logo style={{ height: 28, width: 28 }} />
+        </TouchableOpacity>
+        <Text style={styles.googleSignInText}> Sign in with Google</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#FFF',
+  },
+  title: {
+    fontSize: 48,
+    fontWeight: '500',
+  },
+  googleSignInLogo: {
+    backgroundColor: '#FFF',
+    height: 45,
+    width: 42,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 16,
+    marginLeft: 1.5,
+    borderRadius: 1.5,
+  },
+  googleSignIn: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#4285F4',
+    height: 48,
+    width: 220,
+    borderRadius: 1.5,
+  },
+
+  googleSignInText: {
+    fontFamily: 'Roboto',
+    color: '#FFF',
+    fontSize: 16,
+  },
+});
